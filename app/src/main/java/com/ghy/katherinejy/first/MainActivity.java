866e5f1a0a2a87c392fd9dda18f4a5412840ec07
KatherineJY <kellymayliu@163.com>
2018-12-20 12:57:30 +0800

    stop the project

--- app/src/main/java/com/ghy/katherinejy/first/MainActivity.java
+++ app/src/main/java/com/ghy/katherinejy/first/MainActivity.java
@@ -32,7 +32,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btnAbstain.setOnClickListener(this);
 
     }
-
+/*
     private String doVote(String voteStr){
         String retStr = "";
         try {
@@ -40,7 +40,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             stringBuffer.append("r=").append(URLEncoder.encode(voteStr,"utf-8"));
 
             byte[] data = stringBuffer.toString().getBytes();
-            String urlPath = "http://10.240.6.14:8080/vote/GetVote";
+            String urlPath = "http://192.168.32.1:8080/vote/GetVote";
             URL url = new URL(urlPath);
 
             HttpURLConnection httpURLConnection = (HttpURLConnection)url.openConnection();
@@ -67,6 +67,47 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             Log.i("ii","Exception:" +e.toString());
         }
         Log.i("ii",retStr);
+        return retStr;
+    }
+*/
+
+    private String doVote(String voteStr){
+        String retStr = "";
+        Log.i("vote", "doVote() voteStr:" + voteStr);
+
+        try {
+
+            StringBuffer stringBuffer = new StringBuffer();        //存储封装好的请求体信息
+            stringBuffer.append("r=").append(URLEncoder.encode(voteStr, "utf-8"));
+
+            byte[] data = stringBuffer.toString().getBytes();
+            String urlPath = "http://192.168.32.1:8080/vote/GetVote";
+            URL url = new URL(urlPath);
+
+            HttpURLConnection httpURLConnection = (HttpURLConnection)url.openConnection();
+            httpURLConnection.setConnectTimeout(3000);     //设置连接超时时间
+            httpURLConnection.setDoInput(true);                  //打开输入流，以便从服务器获取数据
+            httpURLConnection.setDoOutput(true);                 //打开输出流，以便向服务器提交数据
+            httpURLConnection.setRequestMethod("POST");     //设置以Post方式提交数据
+            httpURLConnection.setUseCaches(false);               //使用Post方式不能使用缓存
+            //设置请求体的类型是文本类型
+            httpURLConnection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+            //设置请求体的长度
+            httpURLConnection.setRequestProperty("Content-Length", String.valueOf(data.length));
+            //获得输出流，向服务器写入数据
+            OutputStream outputStream = httpURLConnection.getOutputStream();
+            outputStream.write(data);
+
+            int response = httpURLConnection.getResponseCode();            //获得服务器的响应码
+            Log.i("vote", response+"");
+            if(response == HttpURLConnection.HTTP_OK) {
+                InputStream inputStream = httpURLConnection.getInputStream();
+                retStr = inputStreamToString(inputStream);                     //处理服务器的响应结果
+                Log.i("vote", "retStr:" + retStr);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return retStr;
     }
 
